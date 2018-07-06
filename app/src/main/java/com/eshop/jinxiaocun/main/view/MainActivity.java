eaa1b29ece7d742ed3a8479cd21d20df135164a9
wenyongzhe <752664161@qq.com>
2018-07-06 18:00:21 +0800

    主页导航部分代码（不能运行）

--- app/src/main/java/com/eshop/jinxiaocun/main/view/MainActivity.java
+++ app/src/main/java/com/eshop/jinxiaocun/main/view/MainActivity.java
@@ -1,7 +1,10 @@
 package com.eshop.jinxiaocun.main.view;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.eshop.jinxiaocun.base.Application;
 import com.eshop.jinxiaocun.base.BaseActivity;
 import com.eshop.jinxiaocun.thread.AsyncTaskThreadImp;
 import com.eshop.jinxiaocun.thread.ThreadManagerInterface;
@@ -9,26 +12,47 @@ import com.eshop.jinxiaocun.utils.Config;
 import com.eshop.jinxiaocun.utils.MyUtils;
 import com.eshop.jinxiaocun.xiaoshou.bean.TWmSheetMasterBean;
 import com.eshop.jinxiaocun.xiaoshou.dao.TWmSheetMasterDao;
+import com.ycl.tabview.library.TabView;
+import com.ycl.tabview.library.TabViewChild;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import supoin.jinxiaocun.R;
 
 public class MainActivity extends BaseActivity {
 
+    private TabView tabView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
+        tabView= (TabView) findViewById(R.id.tabView);
+        List<TabViewChild> tabViewChildList=new ArrayList<>();
+        TabViewChild tabViewChild01=new TabViewChild(R.drawable.tab01_sel,R.drawable.tab01_unsel, Application.getInstance().getString(R.string.item_home),  FirstFragment.newInstance());
+        TabViewChild tabViewChild02=new TabViewChild(R.drawable.tab02_sel,R.drawable.tab02_unsel, Application.getInstance().getString(R.string.item_single_order),  SingleOrderFrgment.newInstance());
+        TabViewChild tabViewChild03=new TabViewChild(R.drawable.tab04_sel,R.drawable.tab04_unsel, Application.getInstance().getString(R.string.item_collocation_order),  CollocationOrderFrgment.newInstance());
+        TabViewChild tabViewChild04=new TabViewChild(R.drawable.tab03_sel,R.drawable.tab03_unsel, Application.getInstance().getString(R.string.item_report_form), ReportFormFrgment.newInstance());
+        tabViewChildList.add(tabViewChild01);
+        tabViewChildList.add(tabViewChild02);
+        tabViewChildList.add(tabViewChild03);
+        tabViewChildList.add(tabViewChild04);
+        //end add data
+        tabView.setTabViewDefaultPosition(0);
+        tabView.setTabViewChild(tabViewChildList,getSupportFragmentManager());
+        tabView.setOnTabChildClickListener(new TabView.OnTabChildClickListener() {
+            @Override
+            public void onTabChildClick(int  position, ImageView currentImageIcon, TextView currentTextView) {
+//                 Toast.makeText(getApplicationContext(),"position:"+position,Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-        ThreadManagerInterface mThreadManagerInterface = new AsyncTaskThreadImp();
-        mThreadManagerInterface.executeRunnable(this);
 
-
-
-
+        /*ThreadManagerInterface mThreadManagerInterface = new AsyncTaskThreadImp();
+        mThreadManagerInterface.executeRunnable(this);*/
     }
 
 
---
+++ app/src/main/java/com/eshop/jinxiaocun/main/view/XiaoShouFragment.java
@@ -0,0 +1,6 @@
+package com.eshop.jinxiaocun.main.view;
+
+import android.app.Fragment;
+
+public class XiaoShouFragment extends Fragment {
+}

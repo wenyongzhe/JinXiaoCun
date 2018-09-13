19c8924bb27de461736ba72a4267afa87a27e7da
loveanlu <anlu_love@163.com>
2018-09-13 16:40:44 +0800

    批发销售添加接口，添加公共时间选择器

--- app/src/main/java/com/eshop/jinxiaocun/pifaxiaoshou/view/PifaXiaoshouListActivity.java
+++ app/src/main/java/com/eshop/jinxiaocun/pifaxiaoshou/view/PifaXiaoshouListActivity.java
@@ -1,15 +1,33 @@
 package com.eshop.jinxiaocun.pifaxiaoshou.view;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
 import com.eshop.jinxiaocun.R;
 import com.eshop.jinxiaocun.base.INetWorResult;
 import com.eshop.jinxiaocun.base.view.CommonBaseListActivity;
+import com.eshop.jinxiaocun.pifaxiaoshou.adapter.PifaXiaoshouListAdapter;
 import com.eshop.jinxiaocun.pifaxiaoshou.bean.DanJuMainBean;
+import com.eshop.jinxiaocun.pifaxiaoshou.bean.DanJuMainBeanResult;
 import com.eshop.jinxiaocun.pifaxiaoshou.bean.DanJuMainBeanResultItem;
 import com.eshop.jinxiaocun.pifaxiaoshou.presenter.DanJuListImp;
 import com.eshop.jinxiaocun.pifaxiaoshou.presenter.IDanJuList;
+import com.eshop.jinxiaocun.slidedatetimepicker.SlideDateTimeListener;
+import com.eshop.jinxiaocun.slidedatetimepicker.SlideDateTimePicker;
+import com.eshop.jinxiaocun.utils.Config;
+import com.eshop.jinxiaocun.utils.DateUtility;
+import com.eshop.jinxiaocun.widget.AlertUtil;
 import com.eshop.jinxiaocun.widget.RefreshListView;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @Author Lu An
@@ -17,13 +35,15 @@ import java.util.List;
  * 描述
  */
 
-public class PifaXiaoshouListActivity extends CommonBaseListActivity implements INetWorResult{
+public class PifaXiaoshouListActivity extends CommonBaseListActivity implements INetWorResult {
 
-    private List<DanJuMainBeanResultItem> mListInfo;
+
+    private PifaXiaoshouListAdapter mAdapter;
+    private List<DanJuMainBeanResultItem> mListInfo = new ArrayList<>();
     private IDanJuList mDanJuList;
 
-    private int mPageIndex =1;
-    private int mPageSize =20;
+    private int mPageIndex = 1;
+    private int mPageSize = 20;
 
     @Override
     protected int getLayoutContentId() {
@@ -34,7 +54,12 @@ public class PifaXiaoshouListActivity extends CommonBaseListActivity implements 
     protected void initView() {
         super.initView();
 
-        setTopToolBar("批发销售订单列表",R.mipmap.ic_left_light,"",0,"");
+        setTopToolBar("批发销售订单列表", R.mipmap.ic_left_light, "", 0, "");
+
+        setHeaderTitle(R.id.tv_0,R.string.list_item_Status,150);
+        setHeaderTitle(R.id.tv_1,R.string.list_item_ProdName,150);
+        setHeaderTitle(R.id.tv_2,R.string.list_item_ProdCode,150);
+        setHeaderTitle(R.id.tv_3,R.string.list_item_OrderDate,150);
 
         mListView.setonTopRefreshListener(new RefreshListView.OnTopRefreshListener() {
             @Override
@@ -47,10 +72,15 @@ public class PifaXiaoshouListActivity extends CommonBaseListActivity implements 
         mListView.setonBottomRefreshListener(new RefreshListView.OnBottomRefreshListener() {
             @Override
             public void onRefresh() {
-                mPageIndex ++;
+                mPageIndex++;
                 getPifaXiaoshouData();
             }
         });
+
+        mAdapter = new PifaXiaoshouListAdapter(mListInfo);
+        mListView.setOnItemClickListener(this);
+        mListView.setAdapter(mAdapter);
+
     }
 
     @Override
@@ -59,7 +89,7 @@ public class PifaXiaoshouListActivity extends CommonBaseListActivity implements 
         mDanJuList = new DanJuListImp(this);
     }
 
-    private void getPifaXiaoshouData(){
+    private void getPifaXiaoshouData() {
 
         DanJuMainBean mDanJuMainBean = new DanJuMainBean();
         mDanJuMainBean.JsonData.POSID = "";
@@ -73,6 +103,40 @@ public class PifaXiaoshouListActivity extends CommonBaseListActivity implements 
         mDanJuMainBean.JsonData.Page = mPageIndex;
         mDanJuList.getDanJuList(mDanJuMainBean);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        super.onItemClick(parent, view, position, id);
+        AlertUtil.showToast("position " + position);
+        mAdapter.setItemClickPosition(position);
+        mAdapter.notifyDataSetInvalidated();
+    }
+
+//    @OnClick(R.id.dt_startDate)
+//    void OnStartDate(){
+//        new SlideDateTimePicker.Builder(this.getSupportFragmentManager())
+//                .setListener(listenerStart)
+//                .setInitialDate(new Date())
+//                .build()
+//                .show();
+//    }
+//
+//    private SlideDateTimeListener listenerStart = new SlideDateTimeListener() {
+//
+//        @Override
+//        public void onDateTimeSet(Date date) {
+//            Calendar calendar = Calendar.getInstance();
+//            calendar.setTime(date);
+//            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            String strDate = format.format(calendar.getTime());
+//            mTvStartDate.setText(strDate);
+//        }
+//
+//        @Override
+//        public void onDateTimeCancel() {
+//
+//        }
+//    };
 
     @Override
     protected boolean createOrderBefore() {
@@ -116,6 +180,22 @@ public class PifaXiaoshouListActivity extends CommonBaseListActivity implements 
 
     @Override
     public void handleResule(int flag, Object o) {
+        mListView.onRefreshComplete();
+        DanJuMainBeanResult mDanJuMainBeanResult = (DanJuMainBeanResult) o;
+
+        switch (flag) {
+            case Config.MESSAGE_OK:
+                if(mPageIndex==1){
+                    mListInfo = mDanJuMainBeanResult.JsonData;
+                }else{
+                    mListInfo.addAll(mDanJuMainBeanResult.JsonData);
+                }
+                mAdapter.setListInfo(mListInfo);
+                break;
+            case Config.MESSAGE_ERROR:
+                AlertUtil.showToast(o.toString());
+                break;
+        }
 
     }
 

3c0705ecddf6f67642d5f245f50b6601508bce68
loveanlu <anlu_love@163.com>
2019-04-17 22:39:41 +0800

    会员UI的完善

--- app/src/main/java/com/eshop/jinxiaocun/huiyuan/view/ExpenseCheckActivity.java
+++ app/src/main/java/com/eshop/jinxiaocun/huiyuan/view/ExpenseCheckActivity.java
@@ -1,7 +1,19 @@
 package com.eshop.jinxiaocun.huiyuan.view;
 
+import android.widget.TextView;
+
 import com.eshop.jinxiaocun.R;
 import com.eshop.jinxiaocun.base.view.CommonBaseActivity;
+import com.eshop.jinxiaocun.slidedatetimepicker.SlideDateTimeListener;
+import com.eshop.jinxiaocun.slidedatetimepicker.SlideDateTimePicker;
+import com.eshop.jinxiaocun.utils.DateUtility;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Author: 安仔夏天勤奋
@@ -10,6 +22,13 @@ import com.eshop.jinxiaocun.base.view.CommonBaseActivity;
  */
 
 public class ExpenseCheckActivity extends CommonBaseActivity {
+
+
+    @BindView(R.id.tv_start_date)
+    TextView mTvStartDate;
+    @BindView(R.id.tv_end_date)
+    TextView mTvEndDate;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_expense_check;
@@ -18,5 +37,64 @@ public class ExpenseCheckActivity extends CommonBaseActivity {
     @Override
     protected void initView() {
         setTopToolBar("消费查询", R.mipmap.ic_left_light,"",0,"");
+        mTvStartDate.setText(DateUtility.getCurrentDate()+" 00:00:00");
+        mTvEndDate.setText(DateUtility.getCurrentDate()+" 23:59:59");
     }
+
+
+    @OnClick(R.id.tv_start_date)
+    void OnStartDate(){
+        new SlideDateTimePicker.Builder(this.getSupportFragmentManager())
+                .setListener(listenerStart)
+                .setInitialDate(new Date())
+                .build()
+                .show();
+    }
+
+    private SlideDateTimeListener listenerStart = new SlideDateTimeListener() {
+        @Override
+        public void onDateTimeSet(Date date) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String strDate = format.format(calendar.getTime());
+            mTvStartDate.setText(strDate);
+            //TODO 获取选择时间节点后的数据
+        }
+        @Override
+        public void onDateTimeCancel() {
+
+        }
+    };
+
+    @OnClick(R.id.tv_end_date)
+    void OnEndDate(){
+        new SlideDateTimePicker.Builder(this.getSupportFragmentManager())
+                .setListener(listenerEnd)
+                .setInitialDate(new Date())
+                .build()
+                .show();
+    }
+
+    private SlideDateTimeListener listenerEnd = new SlideDateTimeListener() {
+        @Override
+        public void onDateTimeSet(Date date) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String strDate = format.format(calendar.getTime());
+            mTvEndDate.setText(strDate);
+            //TODO 获取选择时间节点后的数据
+        }
+        @Override
+        public void onDateTimeCancel() {
+
+        }
+    };
+
+
+
+
+
+
 }

4be61a29a64fabb43225cd16bfb3da7b75b7fbbf
loveanlu <anlu_love@163.com>
2019-08-15 22:01:38 +0800

    收银对账添加打印，本版升级

--- app/src/main/java/com/eshop/jinxiaocun/reportforms/view/CashierCheckActivity.java
+++ app/src/main/java/com/eshop/jinxiaocun/reportforms/view/CashierCheckActivity.java
@@ -1,19 +1,24 @@
 package com.eshop.jinxiaocun.reportforms.view;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.TextView;
 
 import com.eshop.jinxiaocun.R;
 import com.eshop.jinxiaocun.base.INetWorResult;
 import com.eshop.jinxiaocun.base.view.CommonBaseActivity;
 import com.eshop.jinxiaocun.othermodel.bean.CashierCheckResult;
-import com.eshop.jinxiaocun.othermodel.bean.SalesCheckResult;
 import com.eshop.jinxiaocun.othermodel.presenter.IOtherModel;
 import com.eshop.jinxiaocun.othermodel.presenter.OtherModelImp;
 import com.eshop.jinxiaocun.slidedatetimepicker.SlideDateTimeListener;
 import com.eshop.jinxiaocun.slidedatetimepicker.SlideDateTimePicker;
+import com.eshop.jinxiaocun.utils.AidlUtil;
 import com.eshop.jinxiaocun.utils.CommonUtility;
 import com.eshop.jinxiaocun.utils.Config;
 import com.eshop.jinxiaocun.utils.DateUtility;
+import com.eshop.jinxiaocun.utils.MyUtils;
 import com.eshop.jinxiaocun.widget.AlertUtil;
 
 import java.text.SimpleDateFormat;
@@ -47,6 +52,7 @@ public class CashierCheckActivity extends CommonBaseActivity implements INetWorR
     @Override
     protected void initView() {
         setTopToolBar("收银对账", R.mipmap.ic_left_light, "", 0, "");
+        setTopToolBarRightTitleAndStyle("打印",R.drawable.border_bg);
         mTvStartDate.setText(DateUtility.getCurrentDate()+" 00:00:00");
         mTvEndDate.setText(DateUtility.getCurrentDate()+" 23:59:59");
     }
@@ -115,6 +121,86 @@ public class CashierCheckActivity extends CommonBaseActivity implements INetWorR
     @OnClick(R.id.btn_accountChecking)
     public void onClickAccountChecking(){
         getCashierCheckData();
+    }
+
+    @Override
+    protected void onTopBarRightClick() {
+        if(TextUtils.isEmpty(mTvContent.getText().toString().trim())){
+            AlertUtil.showToast("没有对账内容，不能打印!");
+            return ;
+        }
+
+        if(AidlUtil.getInstance().isConnect()){
+            AlertUtil.showAlert(this, R.string.dialog_title, R.string.is_need_print,
+                    R.string.yes, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            AlertUtil.dismissDialog();
+                            onPrintln();
+                        }
+                    }, R.string.no, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            AlertUtil.dismissDialog();
+                        }
+                    });
+        }else{
+            AlertDialog dialog = new AlertDialog.Builder(this).create();
+            dialog.setTitle("温馨提示");
+            dialog.setMessage("未连接打印机！");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+        }
+
+    }
+
+    //打印
+    private void onPrintln(){
+        int maxLength = 25;
+        String title = "收银对账详情";
+        if(MyUtils.length(title)<maxLength){
+            int beginLength = (maxLength-MyUtils.length(title))>>1;
+            int endLength=beginLength;
+            if(beginLength%2!=0){
+                endLength+=1;
+            }
+            title = MyUtils.rpad(beginLength,"")+title+MyUtils.rpad(endLength,"");
+            AidlUtil.getInstance().printText(title+"\n",30f,false,false);
+        }
+
+        String userName = "操作人: "+ Config.UserName;
+        if(MyUtils.length(userName)<maxLength){
+            int endLength = maxLength-MyUtils.length(userName);
+            userName=userName+MyUtils.rpad(endLength,"");
+            AidlUtil.getInstance().printText(userName+"\n",30f,false,false);
+        }else{//如果超过一行  要换行
+            AidlUtil.getInstance().printText(userName+"\n",30f,false,false);
+            AidlUtil.getInstance().printEmptyLine(1);
+        }
+
+        String exchargeData = "操作时间: "+ DateUtility.getCurrentTime();
+        if(MyUtils.length(exchargeData)<maxLength){
+            int endLength = maxLength-MyUtils.length(exchargeData);
+            exchargeData=exchargeData+MyUtils.rpad(endLength,"");
+            AidlUtil.getInstance().printText(exchargeData+"\n",30f,false,false);
+        }else{//如果超过一行  要换行
+            AidlUtil.getInstance().printText(exchargeData+"\n",30f,false,false);
+            AidlUtil.getInstance().printEmptyLine(1);
+        }
+        AidlUtil.getInstance().printText("========================="+"\n",30f,false,false);
+
+        AidlUtil.getInstance().printText(mTvContent.getText().toString(),30f,false,false);
+
+        AidlUtil.getInstance().printText("========================="+"\n",30f,false,false);
+
+        AidlUtil.getInstance().printEmptyLine(3);
+        AidlUtil.getInstance().print();
     }
 
     //网络返回数据处理
